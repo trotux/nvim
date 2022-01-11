48e148556c4945aa068cfa75e1c2890b5887bf52
Anatoly Nikulin <anatoly.nikulin@gcorelabs.com>
2022-01-11 16:28:04 +0300

    Setup mappings for "hop" plugin

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -108,3 +108,13 @@ nnoremap('<leader>tn', ':NvimTreeToggle<CR>')
 nnoremap('<leader>tr', ':NvimTreeRefresh<CR>')
 nnoremap('<leader>tf', ':NvimTreeFindFile<CR>')
 
+vim.api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
+vim.api.nvim_set_keymap('n', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
+vim.api.nvim_set_keymap('o', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, inclusive_jump = true })<cr>", {})
+vim.api.nvim_set_keymap('o', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, inclusive_jump = true })<cr>", {})
+vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
+vim.api.nvim_set_keymap('', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
+
+nnoremap('<leader>w', "<cmd>HopWord<CR>")
+nnoremap('<leader>l', "<cmd>HopLine<CR>")
+
