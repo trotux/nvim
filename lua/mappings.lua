14420c507deb53535d258924a8167efcdd1390e5
Anatoly Nikulin <anatoly.nikulin@gcorelabs.com>
2022-01-11 17:24:48 +0000

    Add key mapping for "HopChar1" command

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -115,6 +115,7 @@ vim.api.nvim_set_keymap('o', 'F', "<cmd>lua require'hop'.hint_char1({ direction 
 vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
 vim.api.nvim_set_keymap('', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
 
+nnoremap('<leader>c', "<cmd>HopChar1<CR>")
 nnoremap('<leader>w', "<cmd>HopWord<CR>")
 nnoremap('<leader>l', "<cmd>HopLine<CR>")
 
